9727b8b9a4136c424e01375453a47892ff6dc42e
IevgenR <rea2005@ukr.net>
2018-01-12 18:17:15 +0200

    Add files via upload

---
+++ Webtesting/src/WebtestingSeleniumHelpler/PrepareWebdriver.java
@@ -0,0 +1,29 @@
+package WebtestingSeleniumHelpler;
+import  org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Timeouts;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.ie.*;
+public class PrepareWebdriver {
+
+	public  WebDriver driver;
+	protected String webdriverlocation;
+	
+	
+	public WebDriver initwebdriver()
+	{
+		System.setProperty("webdriver.chrome.driver","D:\\temp\\workspace\\New folder\\chromedriver.exe");
+		  //driver = new InternetExplorerDriver ();
+		  WebDriver driver = new ChromeDriver();
+		 
+		  this.driver= driver;
+		  return driver;
+
+	}
+	
+	
+	
+}
